8622fdd808ab71940c7b543a9104a501ffe860ea
ben <b.bouillet@gmail.com>
2024-01-14 21:22:06 +0100

    WIP Home

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <div data-scroll-container>
-        <section id="intro" class="h-screen flex items-center justify-center text-center bg-green-200" data-scroll-section>
+        {/* <section id="intro" class="h-screen flex items-center justify-center text-center bg-green-200" data-scroll-section>
           <div class="container px-5 mx-auto flex flex-col items-center justify-center">
             <Image
               src="/images/logo.png"
@@ -14,7 +14,7 @@ export default function Home() {
               className="relative "
             />
             <div>
-              <h2>Nous découvrir</h2>
+              <h2 class="font-bold">Nous découvrir</h2>
               <div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
@@ -22,8 +22,48 @@ export default function Home() {
               </div>
             </div>
           </div>
+        </section> */}
+        <section class="bg-contain bg-no-repeat bg-[url('/images/logo.png')] bg-gray-700 bg-blend-multiply">
+          <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+            <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Path'O Calme</h1>
+            <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Venez partager vos émotions naturellement.</p>
+            <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+              <a href="#" class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                Nous découvrir
+                <span class="px-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5" />
+                  </svg>
+                </span>
+              </a>
+            </div>
+          </div>
         </section>
-        <section class="py-40" data-scroll-section>...</section>
+        <section class="p-20 grid grid-cols-3 gap-10" data-scroll-section>
+          <div class="max-w-md bg-white border rounded-lg shadow">
+            <a href="#">
+              <img class="rounded-t-lg" src="/images/test.jpg" alt="" />
+            </a>
+            {/* <div class="p-5">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </a>
+    </div> */}
+          </div>
+          <div class="p-5 bg-gray-200 col-span-2">
+            <h2 class="text-6xl">Barbara Bouillet</h2>
+            <p class="py-5 text-xl">Je vous acceuille à <strong>Trept</strong></p>
+            <p></p>
+          </div>
+
+        </section>
         <section id="about-section" class="py-40 bg-green-50" data-scroll-section>...</section>
         <section id="office" class="py-20" data-scroll-section>...</section>
         <section id="services" class="relative py-60 bg-green-50" data-scroll-section>...</section>
