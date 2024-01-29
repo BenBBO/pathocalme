f13015cc04c1c89b5864760a10ba6e6c2700aca1
ben <b.bouillet@gmail.com>
2024-01-29 12:01:39 +0100

    Meilleure gestion de l'opacité

--- src/app/components/prestation/index.js
+++ src/app/components/prestation/index.js
@@ -3,7 +3,7 @@ const Prestation = ({ imageUrl, onClick, title, description, color }) => {
         <article className="mx-auto w-full shadow-xl bg-cover bg-center min-h-150 transform duration-500 md:hover:-translate-y-2 cursor-pointer group"
             style={{ backgroundImage: `url(${imageUrl})` }}
             onClick={onClick}>
-            <div className="p-5 bg-black bg-opacity-75 md:bg-opacity-20 min-h-150 px-10 flex flex-wrap flex-col pt-96 md:hover:bg-opacity-75 transform duration-300">
+            <div className="p-5 bg-black bg-opacity-60 md:bg-opacity-20 min-h-150 px-10 flex flex-wrap flex-col pt-96 md:hover:bg-opacity-60 transform duration-300">
                 <h1 className="text-white text-3xl mb-5 md:transform md:translate-y-20 md:group-hover:translate-y-0 duration-300">
                     {title}
                 </h1>
