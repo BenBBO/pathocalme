c73ca0b6f178b14e3c2858db4c5388f4005fccd0
ben <b.bouillet@gmail.com>
2025-05-12 20:23:36 +0200

    planning mai

--- src/app/page.js
+++ src/app/page.js
@@ -57,20 +57,7 @@ export default function Home() {
         <section className="p-8 bg-event">
           <h1 className="text-2xl font-semibold tracking-widest uppercase text-center mb-8">Planning mensuel</h1>
           <div className="grid grid-cols-1 gap-10">
-            <img className="h-auto max-w-full rounded-lg justify-self-center" loading="lazy" src="images/planning_avril.jpg" alt="programme" />
-          </div>
-        </section>
-        <section className="p-8 bg-event">
-          <h1 className="text-2xl font-semibold tracking-widest uppercase text-center mb-8">Planning Vacances Pâques</h1>
-          <div className="grid grid-cols-1  lg:grid-cols-2  gap-10">
-            <img className="h-auto max-w-full rounded-lg justify-self-center" loading="lazy" src="images/vacance_paque_1.jpg" alt="programme" />
-            <img className="h-auto max-w-full rounded-lg justify-self-center" loading="lazy" src="images/vacance_paque_2.jpg" alt="programme" />
-          </div>
-        </section>
-        <section className="p-8 bg-event">
-          <h1 className="text-2xl font-semibold tracking-widest uppercase text-center mb-8">Évenements à venir</h1>
-          <div className="grid grid-cols-1 gap-10">
-            <img className="h-auto max-w-full rounded-lg justify-self-center" loading="lazy" src="images/animation paques.jpg" alt="programme" />
+            <img className="h-auto max-w-full rounded-lg justify-self-center" loading="lazy" src="images/planning mai.jpg" alt="programme" />
           </div>
         </section>
         <section className="p-8 md:p-14 grid" id="about">
